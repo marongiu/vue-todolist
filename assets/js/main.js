14d1b08096ff6b18cd0183664d2aa683413e9c0d
Andrea <andreamarongiu05@gmail.com>
2021-03-25 15:28:13 +0100

    Creo array di esempi todo

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var app = new Vue ({
+  el: '#root',
+  data: {
+    todoitem: [
+      {
+        item: "Fare spesa"
+      },
+      {
+        item: "Portare il cane fuori"
+      },
+      {
+        item: "Finire esercizio di Boolean"
+      }
+    ]
+  }
+
+});
