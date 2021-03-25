532ab0513118416edb44c6b079262ed9506919f0
Andrea <andreamarongiu05@gmail.com>
2021-03-25 15:43:35 +0100

    Aggiunta oggetto in todos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 var app = new Vue ({
   el: '#root',
   data: {
-    todoitem: [
+    text: '', // valore input
+    todositem: [ // array todo esempi
       {
         item: "Fare spesa"
       },
@@ -12,6 +13,22 @@ var app = new Vue ({
         item: "Finire esercizio di Boolean"
       }
     ]
+  },
+  methods: {
+    add: function () {
+      // se oggetto che vado ad inserire a una lunghezza maggiore ad 1
+      if (this.text.length >= 1) {
+        // creo un nuovo oggetto da pushare in todositem
+        let newitem = {
+            // this.text è il value dell'input
+          item: this.text
+        }
+        // lo pusho in todositem
+        this.todositem.push(newitem)
+        // dopo averlo pushato reimposto il valore come default ''
+        this.text = ''
+      }
+    }
   }
 
 });
